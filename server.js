14868dbebb473cc65cffe987c7c6dbf6a05bbfb0
jnarayanbvg <38253333+jnarayanbvg@users.noreply.github.com>
2020-07-01 16:14:05 -0400

    heroku test

--- server.js
+++ server.js
@@ -26,14 +26,14 @@ app.use(bodyParser.json());
 const comps = require("./routes/api/compRoute")(db);
 app.use("/api/comps", comps);
 
-// // Serve static assets in production
-// if (process.env.NODE_ENV === "production") {
-//    // Set static folder
-//    app.use(express.static("client/build"));
-//    app.get("*", (req, res) => {
-//       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//    });
-// }
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+   // Set static folder
+   app.use(express.static("client/build"));
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));

c55de70f84b0db5f6e2bf7d37f3eb52cedd1bcc6
jnarayanbvg <38253333+jnarayanbvg@users.noreply.github.com>
2020-07-03 10:17:18 -0400

    Initialized react app

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ const comps = require("./routes/api/compRoute")(db);
 app.use("/api/comps", comps);
 
 // Serve static assets in production
-// if (process.env.NODE_ENV === "production") {
-//    // Set static folder
-//    app.use(express.static("client/build"));
-//    app.get("*", (req, res) => {
-//       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//    });
-// }
+if (process.env.NODE_ENV === "production") {
+   // Set static folder
+   app.use(express.static("client/build"));
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));

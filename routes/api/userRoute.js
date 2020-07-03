561fed58d858301626b20b60e5de6c8000d26673
jnarayanbvg <38253333+jnarayanbvg@users.noreply.github.com>
2020-07-02 23:29:01 -0400

    Worked on account verification

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -6,6 +6,8 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const keys = require("../../config/keys");
 const { salt, jsonKey, authExpiry, email } = keys;
+const uuid = require("uuid");
+const validator = require("email-validator");
 
 // Set up mail client
 const nodemailer = require("nodemailer");
@@ -75,6 +77,7 @@ module.exports = (db) => {
                               "User not verified, please see email in associated inbox to verify account",
                         });
                      } else {
+                        /* Handle Token Authentication */
                         const user = {
                            username: sqlUser.Username,
                            teamNumber: sqlUser.TeamNumber,
@@ -97,6 +100,68 @@ module.exports = (db) => {
                               });
                            }
                         );
+                     }
+                  }
+               }
+            );
+         }
+      });
+   });
+
+   // Verify user
+   router.post("/verify/:id", (req, res) => {
+      let sql = `SELECT Username, Password, Verified FROM users WHERE Username = '${req.body.username}' AND VerifyID = '${req.params.id}' LIMIT 1`;
+
+      db.query(sql, (err, result) => {
+         if (err) {
+            res.status(404).send(err);
+         }
+
+         // Check if username exists
+         if (!result.length) {
+            res.status(404).send({
+               message: "Invalid username and/or verification ID",
+            });
+         } else {
+            // Get user object
+            const sqlUser = result[0];
+
+            // Check password
+            bcrypt.compare(
+               req.body.password,
+               sqlUser.Password,
+               (err, result) => {
+                  if (err)
+                     res.status(404).send({
+                        message: "Failed to check against password",
+                        err,
+                     });
+
+                  // Check if password authenticated
+                  if (!result) {
+                     res.status(404).send({
+                        message: "Incorrect password",
+                     });
+                  } else {
+                     // Check if verified
+                     if (!sqlUser.Verified) {
+                        let sql = `UPDATE users SET Verified = 1 WHERE Username = '${req.body.username}'`;
+                        db.query(sql, (err) => {
+                           if (err) {
+                              res.status(400).send({
+                                 message:
+                                    "Failed to update verification status",
+                                 err,
+                              });
+                           }
+                           res.status(200).send({
+                              message: "Successfully verified account",
+                           });
+                        });
+                     } else {
+                        res.status(200).send({
+                           message: "Account already verified",
+                        });
                      }
                   }
                }
@@ -131,32 +196,53 @@ module.exports = (db) => {
                if (err) {
                   res.status(404).send(err);
                }
-               if (result.length) {
+
+               // Check for valid email
+               if (!validator.validate(req.body.email)) {
+                  res.status(400).send({ message: "Invalid email" });
+               }
+               // Check for duplicate username
+               else if (result.length) {
                   res.status(409).send({
                      message: "A user with that username already exists",
                   });
-               } else {
-                  // Send database query
-                  let sql = `INSERT INTO users (Username, Password, AdminKey, Email, TeamNumber, VerifyID, Verified) VALUES ('${req.body.username}', '${passHash}', '${keyHash}', '${req.body.email}', '${req.body.teamNumber}', '${req.body.verifyID}', 0)`;
+               }
+               // Send database insertion query
+               else {
+                  const verifyID = uuid.v4();
+                  let sql = `INSERT INTO users (Username, Password, AdminKey, Email, TeamNumber, VerifyID, Verified) VALUES ('${req.body.username}', '${passHash}', '${keyHash}', '${req.body.email}', '${req.body.teamNumber}', '${verifyID}', 0)`;
                   db.query(sql, (err) => {
                      if (err) {
                         res.status(404).send(err);
                      }
 
-                     // Send email
+                     const verifyLink = `https://www.testing.team7558.com?verifyID=${verifyID}`;
                      const sendOptions = mailOptions;
                      sendOptions.to = req.body.email;
                      sendOptions.subject =
                         "Account Registered at scouting.team7558.com!";
-                     sendOptions.text = req.body.username;
+                     sendOptions.text = `Hello ${req.body.username} from Team ${req.body.teamNumber}!\n
+                     Thank you for registering an account with scouting.team7558.com! To use our web application for gathering and using your own scouting data, please verify your account at this link: ${verifyLink}\n
+                     Please take note of your account information, as it cannot be changed later:
+                     Username: ${req.body.username}
+                     Password: ${req.body.password}
+                     Admin Key: ${req.body.adminKey}
+                     Email: ${req.body.email}
+                     Team Number: ${req.body.teamNumber}\n
+                     Thank you again for registering and we hope you'll be scouting soon!
+                     - Alt-F4's Scouting and Strategy Department`;
+
                      transport.sendMail(sendOptions, (err, info) => {
                         if (err)
                            res.status(404).send({
                               message: "Failed to send email",
                               err,
+                              info,
                            });
                         else {
-                           res.status(201).send(info);
+                           res.status(201).send({
+                              message: "Verification email successfully sent",
+                           });
                         }
                      });
                   });

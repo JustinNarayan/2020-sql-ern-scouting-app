b5ab279bb4e6e3daf9fc6de321f20246e2784bcf
jnarayanbvg <38253333+jnarayanbvg@users.noreply.github.com>
2020-07-03 18:55:21 -0400

    Worked on login front end

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import model from "./models/model";
 import { StoreProvider, createStore } from "easy-peasy";
 import { BrowserRouter } from "react-router-dom";
 import Routing from "./components/Routing";
-import "./assets/style.css";
+import "./assets/css/style.css";
 
+// Create application-level state
 const store = createStore(model);
 
 function App() {
--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,15 +1,90 @@
-import React, { Fragment } from "react";
+import React from "react";
+import {
+   Card,
+   Label,
+   CardBody,
+   CardImg,
+   Form,
+   FormGroup,
+   Button,
+   Input,
+} from "reactstrap";
 import QueryString from "query-string";
 import PropTypes from "prop-types";
 
 const Login = ({ location: { search } }) => {
    const query = QueryString.parse(search);
    return (
-      <Fragment>
-         <h1>Hello this is a test page</h1>
-         <p></p>
-      </Fragment>
+      <Card className={classes.card} style={styles.card}>
+         <Label
+            className={classes.labelTitle}
+            style={styles.labelTitle}
+            for='formUsername'>
+            TEAM 7558 SCOUTING APP
+         </Label>
+         <img src='../../assets/photos/infiniterechargelogo.png' />
+         <Form className={classes.form}>
+            <CardBody className={classes.cardBody} style={styles.cardBody}>
+               <FormGroup className={classes.formGroup}>
+                  <Input
+                     type='text'
+                     name='username'
+                     id='formUsername'
+                     className={classes.input}
+                     placeholder='Username'
+                  />
+               </FormGroup>
+               <FormGroup className={classes.formGroup}>
+                  <Input
+                     type='password'
+                     name='password'
+                     id='formPassword'
+                     className={classes.input}
+                     placeholder='Password'
+                  />
+               </FormGroup>
+               <Button
+                  className={classes.button}
+                  style={styles.button}
+                  color='login-light'
+                  block
+                  size='lg'
+                  outline
+                  onClick={() => console.log()}>
+                  Login
+               </Button>
+            </CardBody>
+         </Form>
+      </Card>
    );
+};
+
+const classes = {
+   card: "bg-login-form text-center my-4 mx-auto ",
+   labelTitle: "bg-login-form text-login-light",
+   cardBody: "bg-login-form p-0",
+   formGroup: "bg-login-form",
+   input: "text-login-form",
+   label: "mb-0",
+};
+
+const styles = {
+   card: {
+      borderRadius: "12px",
+      padding: "24px",
+      width: "480px",
+   },
+   cardBody: {
+      borderRadius: "12px",
+   },
+   labelTitle: {
+      fontWeight: "400",
+      fontSize: "32px",
+      marginBottom: "32px",
+   },
+   button: {
+      marginTop: "32px",
+   },
 };
 
 Login.propTypes = {
